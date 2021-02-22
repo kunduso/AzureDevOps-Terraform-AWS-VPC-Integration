be8220b61ccd99c03a46a90ee6c3f39e4439c2fd
Sourav Kundu <skundu.dev@gmail.com>
2021-02-22 14:40:10 -0600

    added referenced variables

--- variable.tf
+++ variable.tf
@@ -5,3 +5,11 @@ variable "region" {
 variable "network_address_space" {
   default = "10.1.0.0/16"
 }
+variable "access_key" {
+  description = "The access_key that belongs to the IAM user"
+  type        = string
+}
+variable "secret_key" {
+  description = "The secret_key that belongs to the IAM user"
+  type        = string
+}
