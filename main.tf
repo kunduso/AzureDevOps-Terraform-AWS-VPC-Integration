6f24e3ca7942340b9ee8c8b23a54f2e10741e43b
Sourav Kundu <skundu.dev@gmail.com>
2021-02-25 14:24:58 -0600

    added code to provision subnet

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ resource "aws_vpc" "myvpc" {
     Environment = "Dev"
   }
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/availability_zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "mysubnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.myvpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_subnet" "mysubnet2" {
+  cidr_block              = var.subnet2_address_space
+  vpc_id                  = aws_vpc.myvpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+}
