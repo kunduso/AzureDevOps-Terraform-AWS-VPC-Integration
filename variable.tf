c13c569e1d2a3d8d3dfc0f128fb23c13df2f5645
Sourav Kundu <skundu.dev@gmail.com>
2021-02-25 14:34:19 -0600

    added subnet address spaces

--- variable.tf
+++ variable.tf
@@ -15,3 +15,9 @@ variable "secret_key" {
   type        = string
   sensitive   = true
 }
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
